c2f8ff70feb66894ac50b2fbbc3783195d89e1fc
MVIUdani <imeudani@gmail.com>
2021-08-05 17:47:18 +0700

    create crud application

---
+++ CRUD_App/php/DBConfig.php
@@ -0,0 +1,18 @@
+<?php
+
+//Define your host here.
+$HostName = "localhost";
+
+//Define your database name here.
+$DatabaseName = "crud_application";
+
+//Define your database username here.
+$HostUser = "root";
+
+//Define your database password here.
+$HostPass = " ";
+
+//Define your database port number here.
+$HostPort = "3308";
+
+?>
